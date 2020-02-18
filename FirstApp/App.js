2da5a307b4defa0726574cb43c14b76bb82acaef
Sandhya Yadav <yadavshndhya@gmail.com>
2020-02-18 15:41:15 +0530

    flatlist

--- FirstApp/App.js
+++ FirstApp/App.js
@@ -1,72 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
-  ScrollView,
-  View,
   Text,
-  Button,
+  View,
   TextInput,
-  
+  Button,
+  ScrollView,
+  FlatList
 } from 'react-native';
-import { useState } from 'react';
-
-
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
-  const [courseGoals, setCourseGoal] = useState([]);
+  const [courseGoals, setCourseGoals] = useState([]);
 
-  function goalInputHandler(enteredText){
+  const goalInputHandler = enteredText => {
     setEnteredGoal(enteredText);
+  };
 
-  }
   const addGoalHandler = () => {
-    setCourseGoal(currentGoals => [...currentGoals, enteredGoal ]);
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: enteredGoal }
+    ]);
   };
 
   return (
-    <View style=
-    {styles.screen}>
-      <View style={styles.root}>
-        <TextInput 
-        placeholder="Goal"
-        style={styles.input}
-        onChangeText={goalInputHandler}
-        value={enteredGoal}/>
-        <Button title="ADD" onPress={addGoalHandler}/>
-
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <ScrollView>
-        {courseGoals.map((goal) => 
-        <View  key={goal}
-        style={styles.listItem}>
-          <Text>{goal}</Text></View>)}
-      </ScrollView>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
-};
+}
+
 const styles = StyleSheet.create({
   screen: {
-    padding: 40
-
+    padding: 50
   },
-  root: {
-    flexDirection: "row", 
-    justifyContent: "space-between",
-    alignItems: "center"
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
   input: {
-    width: '80%', 
-    borderColor: 'black', 
-    borderWidth: 1, 
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
     padding: 10
   },
   listItem: {
@@ -76,7 +70,4 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1
   }
-
 });
-
-
